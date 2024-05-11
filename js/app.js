672bcff9c422aaa298c723b6a222daff11f56a73
SamuelESuarezE <151666245+SamuelESuarezE@users.noreply.github.com>
2024-05-11 13:30:15 -0500

    feat: :sparkles: Central section search bar for songs completed

--- js/app.js
+++ js/app.js
@@ -2,6 +2,39 @@ import { MusicCard } from "./components/MusicCard.js";
 import { SearchBar } from "./components/SearchBar.js";
 import { CurrentTrack } from "./components/CurrentTrack.js";
 import { searchUniqueTrack, searchTracks, searchAlbums } from "./modules/search.js";
-await searchUniqueTrack()
-await searchTracks()
-await searchAlbums()
+// await searchUniqueTrack()
+// await searchTracks()
+// await searchAlbums()
+
+const searchBars = document.querySelectorAll("search-bar");
+// console.log(searchBars[0].shadowRoot.children[2].children[1])
+
+
+searchBars[0].shadowRoot.children[2].children[1].addEventListener("keyup", async function(e) {
+    if (e.key === "Enter") {
+        let query = e.target.value
+        let dataSongs = await searchTracks(query)
+        let albumContainer = document.querySelector('.albumContainer')
+        for (let song in dataSongs) {
+            let musicCard = document.createElement("music-card")
+            musicCard.setAttribute("uri", dataSongs[song])
+            albumContainer.appendChild(musicCard)
+        }
+    }
+
+})
+
+// Evento busqueda current track
+searchBars[1].shadowRoot.children[2].children[1].addEventListener("keyup", async function(e) {
+    if (e.key === "Enter") {
+        let query = e.target.value
+        let dataSong = await searchUniqueTrack(query)
+        let currentTrackFrame = document.querySelector('current-track-frame')
+        let currentTrackContainer = document.querySelector('#currentTrack')
+        currentTrackFrame.remove()
+        
+        currentTrackFrame.setAttribute("uri", dataSong)
+        currentTrackContainer.appendChild(currentTrackFrame)
+    }
+
+})
--- js/modules/search.js
+++ js/modules/search.js
@@ -1,12 +1,44 @@
 
 export const searchUniqueTrack = async(query) => {
-    let res = await (await fetch("http://localhost:5503/tracks")).json()
-    let song = res.items[0].data.uri
-    console.log(song)
-    return song
+    let url = `https://spotify23.p.rapidapi.com/search/?q=${query}&type=tracks&offset=0&limit=1&numberOfTopResults=0`;
+    let options = {
+        method: 'GET',
+        headers: {
+            'x-rapidapi-key': 'ea6cd4dd0bmsh8413380809d711bp1b1736jsn8db2d953d8ab',
+            'x-rapidapi-host': 'spotify23.p.rapidapi.com',
+            'Content-Type': 'application/json'
+        }
+    };
+
+    try {
+        let response = await fetch(url, options);
+        let result = await response.json();
+        let dataUpdate = result.tracks.items[0].data.uri
+        return dataUpdate
+    } catch (error) {
+        console.error(error);
+    }
 }
 
 export const searchTracks = async(query) => {
+    const url = `https://spotify23.p.rapidapi.com/search/?q=${query}&type=albums&offset=0&limit=6&numberOfTopResults=0`;
+    const options = {
+        method: 'GET',
+        headers: {
+            'x-rapidapi-key': 'ea6cd4dd0bmsh8413380809d711bp1b1736jsn8db2d953d8ab',
+            'x-rapidapi-host': 'spotify23.p.rapidapi.com',
+            'Content-Type': 'application/json'
+        }
+    };
+
+    try {
+        const response = await fetch(url, options);
+        const result = await response.json();
+        console.log(result);
+    } catch (error) {
+        console.error(error);
+    }
+
     let res = await (await fetch("http://localhost:5502/tracks")).json()
     let dataSongs = []
     res.items.forEach(item => {
