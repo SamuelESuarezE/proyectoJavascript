ae6cab822054689c0d68b3c91265c4a7a5616695
SamuelESuarezE <151666245+SamuelESuarezE@users.noreply.github.com>
2024-05-16 12:30:36 -0500

    chore: :recycle: 3rd try to fix searchbar style not working on github pages

--- js/components/SearchBar.js
+++ js/components/SearchBar.js
@@ -7,7 +7,49 @@ export class SearchBar extends HTMLElement {
 
     render() {
         this.shadowRoot.innerHTML = /*html*/`
-            <link rel="stylesheet" href="./js/components/SearchBar.css">
+            <style>
+
+
+/* ARTICLE */
+.searchFigure i{
+    color: #bbbbbb;
+    position: absolute;
+    top: 24px;
+    left: 12px;
+}
+
+.searchFigure:hover > i{
+    color: white;
+}
+
+
+.searchFigure {
+    position: relative;
+    height: 28px;
+    width: 100%
+
+}
+
+/* INPUT TEXT */
+
+.searchBar {
+    width: calc(100% - 72px);
+    background-color: #242424;
+    border: 0;
+    border-radius: 500px;
+    color: #fff;
+    height: 100%;
+    padding: 6px 36px;
+    margin-block: 12px;
+    text-overflow: ellipsis;
+}
+
+.searchBar:hover {
+    box-shadow: 0px 0px 1.5px rgb(197, 197, 197);
+    background-color: #282828;
+}
+
+            </style>
             <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css">
 
             <article class="searchFigure">
